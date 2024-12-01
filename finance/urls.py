499ff2a4ed9a1df37af3f234b7050ba1d9545ae3
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-12-01 18:11:27 -0300

    test: a test chart view has been created

--- finance/urls.py
+++ finance/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from finance.views import HomeView
+from finance.views import HomeView, ChartView
 
 app_name = 'finance'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('dashboard/', ChartView.as_view(), name='dashboard'),
 
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,7 @@
+import os
+from django.views import View
+import matplotlib.pyplot as plt
+import pandas as pd
 from django.shortcuts import render
 from django.views.generic import DetailView
 
@@ -7,3 +11,27 @@ class HomeView(DetailView):
 
     def get(self, *args, **kwargs):
         return render(self.request, self.template_name)
+
+
+class ChartView(View):
+    def get(self, request):
+
+        data = {
+            'Category': ['A', 'B', 'C', 'D'],
+            'Values': [10, 20, 15, 30]
+        }
+
+        df = pd.DataFrame(data)
+
+        plt.figure(figsize=(8, 5))
+        df.plot(kind='bar', x='Category', y='Values', legend=False)
+        plt.title('Sample Bar Chart')
+        plt.xlabel('Category')
+        plt.ylabel('Values')
+
+        chart_path = 'analytics/static/analytics/chart.png'
+        os.makedirs(os.path.dirname(chart_path), exist_ok=True)
+        plt.savefig(chart_path)
+        plt.close()
+
+        return render(request, 'finance/dashboard.html')
