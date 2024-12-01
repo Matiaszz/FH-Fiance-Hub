1ff0b9394c7d3a3467315dce1e9ad55b202a4bc2
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-30 22:59:50 -0300

    feat(navbar): switch theme added

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,37 @@
+
 const navbar = document.querySelector('.navbar');
+const themeSwitcherLink = document.getElementById('theme-switcher-link');
+const themeIcon = document.getElementById('theme-icon');
+const content = document.querySelector('.content');
+
+function switcher() {
+    
+    console.log("Botão clicado!"); 
+    
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    } else {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    }
+    
+}
 
 navbar.addEventListener('mouseenter', () => {
     const texts = document.querySelectorAll('.text');
     texts.forEach(text => {
-        text.style.display = 'inline'; 
+        text.style.display = 'inline';
     });
 });
 
 navbar.addEventListener('mouseleave', () => {
     const texts = document.querySelectorAll('.text');
     texts.forEach(text => {
-        text.style.display = 'none'; 
+        text.style.display = 'none';
     });
 });
